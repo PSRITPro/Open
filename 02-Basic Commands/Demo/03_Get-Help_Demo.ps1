dc784da92c645808ad668d37b0773b4a9f5f11b7
Siva Sankara Padala <pssreddy01@gmail.com>
2026-04-06 12:59:35 +0530

    Commit

--- 02-Basic Commands/Demo/03_Get-Help_Demo.ps1
+++
@@ -1,9 +0,0 @@
-#Get help for specific cmdlet
-Get-Help Get-Verb
-
-#help content in a separate searchable window
-Get-Help Get-Verb -ShowWindow
-Get-Help about_commonparameters -ShowWindow
-
-#Get online help
-Get-Help Get-Verb -Online
